02f41f4868f8184711ae44d488fbd7404334ec71
rsbear <hellorosss@gmail.com>
2025-11-13 19:40:45 -0600

    add postcss to deps

--- build_npm.ts
+++ build_npm.ts
@@ -19,7 +19,7 @@ await build({
   test: false,
   package: {
     name: "bundlemeup",
-    version: "0.1.20",
+    version: "0.1.21",
     description:
       "A CLI tool that wraps rsbuild to bundle React, Svelte, or Preact to specified targets",
     license: "MIT",
