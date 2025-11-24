03daba8c431de5fe2d625698437a95b18f32a559
rsbear <hellorosss@gmail.com>
2025-11-24 17:47:12 -0600

    v bump

--- build_npm.ts
+++ build_npm.ts
@@ -19,7 +19,7 @@ await build({
   test: false,
   package: {
     name: "bundlemeup",
-    version: "0.2.18",
+    version: "0.2.21",
     description:
       "A CLI tool that wraps rsbuild to bundle React, Svelte, or Preact to specified targets",
     license: "MIT",
